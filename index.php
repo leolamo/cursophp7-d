a3528679a2ffc37a70aaa945126a91e7a46859e9
Leonardo <lamo2802@hotmail.com>
2020-11-03 12:53:19 -0300

    Classe Usuario criada

--- index.php
+++ index.php
@@ -2,10 +2,20 @@
 
 require_once("config.php");
 
+$root = new Usuario();
+
+$root->loadById(1);
+
+echo $root;
+
+
+
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * from tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
 ?>
